7127d95e11567765cc36425d65543ae5b8f8d326
Joe-noh <goflb.jh@gmail.com>
2019-04-27 09:40:16 +0900

    初期値のテストになってないじゃん

--- test/stores.test.js
+++ test/stores.test.js
@@ -2,51 +2,55 @@ import { todos } from '../src/stores.js'
 import { get } from 'svelte/store'
 
 describe('todos store', () => {
-  beforeEach(() => {
-    todos.clear()
+  describe('initial value', () => {
+    test('is an empty array', () => {
+      expect(get(todos)).toEqual([])
+    })
   })
 
-  test('initial value is an empty array', () => {
-    expect(get(todos)).toEqual([])
-  })
+  describe('manipulations', () => {
+    beforeEach(() => {
+      todos.clear()
+    })
 
-  test('add', () => {
-    todos.add('Something has to be done')
+    test('add', () => {
+      todos.add('Something has to be done')
 
-    const [added] = get(todos)
+      const [added] = get(todos)
 
-    expect(added.done).toBe(false)
-    expect(added.title).toBe('Something has to be done')
-    expect(added.id).toEqual(expect.any(String))
-  })
+      expect(added.done).toBe(false)
+      expect(added.title).toBe('Something has to be done')
+      expect(added.id).toEqual(expect.any(String))
+    })
 
-  test('toggle', () => {
-    todos.add('Something you must toggle')
-    todos.toggle(get(todos)[0].id)
+    test('toggle', () => {
+      todos.add('Something you must toggle')
+      todos.toggle(get(todos)[0].id)
 
-    const [toggled] = get(todos)
+      const [toggled] = get(todos)
 
-    expect(toggled.done).toBe(true)
-  })
+      expect(toggled.done).toBe(true)
+    })
 
-  test('remove', () => {
-    todos.add('Buy something great')
-    todos.add('Buy something good')
+    test('remove', () => {
+      todos.add('Buy something great')
+      todos.add('Buy something good')
 
-    expect(get(todos).length).toEqual(2)
+      expect(get(todos).length).toEqual(2)
 
-    todos.remove(get(todos)[0].id)
+      todos.remove(get(todos)[0].id)
 
-    expect(get(todos).length).toEqual(1)
-  })
+      expect(get(todos).length).toEqual(1)
+    })
 
-  test('clear', () => {
-    todos.add('Buy something great')
-    todos.add('Buy something good')
+    test('clear', () => {
+      todos.add('Buy something great')
+      todos.add('Buy something good')
 
-    todos.clear()
+      todos.clear()
 
-    expect(get(todos)).toEqual([])
+      expect(get(todos)).toEqual([])
 
+    })
   })
 })
